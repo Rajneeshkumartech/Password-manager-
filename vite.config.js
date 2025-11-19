280ddbc6da49e4e632b237c9bbfe61ae1a7323db
rajneeshkumartech <rajneeshkumar.tech@gmail.com>
2025-11-20 02:03:56 +0530

    Fix: Set base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 const repoName = '/Password-manager-/'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [ tailwindcss(),react(),],
+
   base: repoName,
+  plugins: [ tailwindcss(),react(),],
   
 })
