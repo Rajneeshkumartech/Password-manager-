194f9afa8c063620d7d634c8311d323d1c0ff5ed
rajneeshkumartech <rajneeshkumar.tech@gmail.com>
2025-11-20 01:58:07 +0530

    provide base reponame

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
+const repoName = '/Password-manager-/'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [ tailwindcss(),react(),],
+  base: repoName,
   
 })
